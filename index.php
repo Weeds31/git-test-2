78373a60097a06c46ceee69b70d4f9da8e06a077
Weeds31 <hmdsholihudin@gmail.com>
2021-09-17 11:32:36 +0700

    menambahkan list di index.php

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio ut ullam ratione, ab numquam velit commodi dolore vitae consequatur blanditiis, repellendus repudiandae laboriosam cum cupiditate natus fugit? Quasi, nemo. Beatae?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
